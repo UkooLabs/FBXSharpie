5d6ea139651894599c65f636aa933fc284742b8a
Peter Tribe <peter.tribe@ukoolabs.com>
2019-11-29 12:18:03 -0800

    Update: api version

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,4 @@
 if ($env:NugetPushApiToken -ne $null)
 {
-	dotnet nuget push -k $Env:NugetPushApiToken artifacts\*.nupkg -s https://www.nuget.org/api/v2/package
-	dotnet nuget push -k $Env:NugetPushApiToken artifacts\*.snupkg -s https://www.nuget.org/api/v2/package
+	dotnet nuget push -k $Env:NugetPushApiToken artifacts\*.nupkg -s https://www.nuget.org/api/v3/package
 }

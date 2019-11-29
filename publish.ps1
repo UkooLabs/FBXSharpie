fd9c753f006373f5fc60d8b5f5d58edf9a1895dc
Peter Tribe <peter.tribe@ukoolabs.com>
2019-11-29 11:53:04 -0800

    Added: snupkg

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,5 @@
 if ($env:NugetPushApiToken -ne $null)
 {
 	dotnet nuget push -k $Env:NugetPushApiToken artifacts\*.nupkg -s https://www.nuget.org/api/v2/package
+	dotnet nuget push -k $Env:NugetPushApiToken artifacts\*.snupkg -s https://www.nuget.org/api/v2/package
 }

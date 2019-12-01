6b71aa948b910b074f252adde284a0356de6930c
Peter Tribe <peter.tribe@ukoolabs.com>
2019-12-01 13:36:12 -0800

    Update: publish

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
 if ($env:NugetPushApiToken -ne $null)
 {
-	dotnet nuget push -k $env:NugetPushApiToken artifacts\*.nupkg -s https://api.nuget.org/v3/index.json
+	.\nuget push artifacts\*.nupkg $env:NugetPushApiToken -source https://api.nuget.org/v3/index.json
 }
